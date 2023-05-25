5cc926150cdbd1db5dab0ddad16862ee3b838619
Thayna Marostica <116041441+thayM@users.noreply.github.com>
2023-05-25 11:46:36 +0000

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  calendario = {
+    mes: 'Maio',
+    ano: 2023,
+    foto: 'https://images.pexels.com/photos/15625818/pexels-photo-15625818/free-photo-of-cidade-meio-urbano-sombrio-escuro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    dias_da_semana: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+    primeira_semana: ['', 1, 2, 3, 4, 5, 6],
+    segunda_semana: [7, 8, 9, 10, 11, 12, 13],
+    terceira_semana: [14, 15, 16, 17, 18, 19, 20],
+    quarta_semana: [21, 22, 23, 24, 25, 26, 27],
+    quinta_semana: [28, 29, 30, 31, '', '', '']
+  }
 }
