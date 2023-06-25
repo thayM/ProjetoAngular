43e0a132ff35e9370fb6a5d3c4dd3e953b060b21
Pedro Aguiar <96843666+aguiarhub@users.noreply.github.com>
2023-06-25 19:35:34 +0000

    funcionalidades da calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { reverse } from 'dns';
 
 @Component({
   selector: 'app-home',
@@ -10,24 +11,100 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage {
   tela : any = ""
+  resultado: any = ""
+  //Funções uteis
+  //Inverter a ordem de uma String
+  inverterString(string: string) {
+    let textoInvertido = '';
+    for (var i = string.length - 1; i >= 0; i--) {
+        textoInvertido += string[i];
+    }
+    console.log(textoInvertido)
+    return textoInvertido;
+  }
+  //Funções basicas da calculadora
 
+  //Resetar a calculadora
+  limparTela(){
+    this.tela = ""
+    this.resultado = ""
+  }
+
+  //Apagar o ultimo Algoritmo 
+  del() {
+    let resultNumber = this.tela;
+    this.tela = resultNumber.substring(0, resultNumber.length - 1);
+  }
+
+  //Adicionar Tecla dentro da expressão numerica
   setTecla(num: any){
     this.tela += num
   }
+
+  //Funções para calculo 
   calc(){
     if (this.tela != "") {
       let calculo:any = this.tela
-      this.tela = eval(calculo);
+      this.tela+= "="
+      this.resultado = eval(calculo);
   } else {
       this.tela = ""
   }
   }
-  limparTela(){
-    this.tela =""
-  }
-  del() {
-    let resultNumber = this.tela;
-    this.tela = resultNumber.substring(0, resultNumber.length - 1);
-}
 
+  trocaSinal(){
+  let array = this.tela.length
+  let numParaTrocarSinalInvertido = " "
+  let qtdAlgori = 0
+  //Ultimo numero digitado, OBS: qtdAlgori é o tamanho do ultimo numero para futura substituição
+    for (let index = array-1; index >= 0; index--) {
+      if(this.tela[index] != "-" && this.tela[index] != "+" && this.tela[index] != "/" && this.tela[index] != "*"){
+        numParaTrocarSinalInvertido += `${this.tela[index]}`
+        qtdAlgori++
+      }else{
+        index = 0
+      }
+    }
+    //Trocando o Sinal do numero desejado dentro da expressão numerica
+    if(qtdAlgori != 0){
+      let numParaTrocarSinal = eval(`${this.inverterString(numParaTrocarSinalInvertido)} *(-1)`)
+      let novoCalc = `${this.tela.substring(0,(array-qtdAlgori))}(${numParaTrocarSinal})`
+      this.tela = novoCalc 
+   }else{
+      let novoCalc = eval(`${this.tela}*-1`)
+      this.tela = novoCalc
+   }
+  }
+
+  porcentagem(){
+  let array = this.tela.length
+  let numParaPorcentagemInvertido = ""
+  //tamanho do ultimo numero para futura substituição 
+  let qtdAlgori = 0
+  for (let index = array-1; index >= 0; index--) {
+    if(this.tela[index] != "-" && this.tela[index] != "+" && this.tela[index] != "/" && this.tela[index] != "*"){
+      qtdAlgori++
+    }else{
+      index = 0
+    }
+  }
+  //identificando os numeros da equação 
+  for (let index = array-1; index >= 0; index--) {
+      if(this.tela[index] != "-" && this.tela[index] != "+" && this.tela[index] != "/" && this.tela[index] != "*"){
+        numParaPorcentagemInvertido += `${this.tela[index]}`
+      }else{
+        numParaPorcentagemInvertido +=","
+      }
+  }
+  //Substituindo o valor da porcentagem dentro da expressão numerica
+  let numeros = numParaPorcentagemInvertido.split(",")
+    if (numeros.length <= 1) {
+      this.tela = "SintaxError"
+    }
+    else{
+      let numParaPorcentagem = eval(`(${this.inverterString(numeros[0])}*${this.inverterString(numeros[1])})/100`)
+      let novoCalc = `${this.tela.substring(0,(array-qtdAlgori))}(${numParaPorcentagem})`
+      this.tela = novoCalc 
+    }
+  }
 }
